785269ddd907bbee5680de9e50837ada9ec23d80
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-02-05 13:34:30 +0100

    Using microbundle

--- example/index.jsx
+++ example/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import useAbortableFetch from '../src';
+import useAbortableFetch from '..';
 
 const Joke = ({ url }) => {
   const { data: joke, loading, error, abort } = useAbortableFetch(url);
