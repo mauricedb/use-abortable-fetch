8555ed3b0ae9efee8feca275a0e1022a41f24940
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-11-10 09:11:20 +0100

    Cleanup of test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,16 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import React from 'react';
-import { render, cleanup, waitForElement } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import fetch from 'jest-fetch-mock';
-
-// Note: Both tests still produce the following error message
-//   Warning: An update to LoadData inside a test was not wrapped in act(...).
 
 import useAbortableFetch from './index';
 
-(global as any).fetch = fetch;
-afterEach(cleanup);
+global.fetch = fetch;
 
 const LoadData = (): JSX.Element => {
   const { data, loading, error } = useAbortableFetch('http://some-server');
@@ -27,37 +21,37 @@ const LoadData = (): JSX.Element => {
 test('Renders the data after success', async (): Promise<void> => {
   (fetch as any).mockResponseOnce(JSON.stringify({ x: 1 }), {
     headers: {
-      'content-type': 'application/json'
-    }
+      'content-type': 'application/json',
+    },
   });
 
-  const { getByText } = render(<LoadData />);
+  render(<LoadData />);
 
-  getByText('Loading: true');
-  await waitForElement((): any => getByText('Loading: false'));
+  screen.getByText('Loading: true');
+  await screen.findByText('Loading: false');
 
-  getByText('Data: {"x":1}');
-  getByText('Error: null');
+  screen.getByText('Data: {"x":1}');
+  screen.getByText('Error: null');
 });
 
 test('Renders the error after failure', async (): Promise<void> => {
   (fetch as any).mockResponseOnce(
     JSON.stringify({
-      x: 2
+      x: 2,
     }),
     {
       status: 404,
       headers: {
-        'content-type': 'application/json'
-      }
+        'content-type': 'application/json',
+      },
     }
   );
 
-  const { getByText } = render(<LoadData />);
+  render(<LoadData />);
 
-  getByText('Loading: true');
-  await waitForElement((): any => getByText('Loading: false'));
+  screen.getByText('Loading: true');
+  await screen.findByText('Loading: false');
 
-  getByText('Data: {"x":2}');
-  getByText('Error: {"status":404}');
+  screen.getByText('Data: {"x":2}');
+  screen.getByText('Error: {"status":404}');
 });
